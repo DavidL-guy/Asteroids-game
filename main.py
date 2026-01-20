b9f6516918e34b0d3f92d42b69cc24575e236a1f
DavidL-guy <davidlgan392@gmail.com>
2026-01-20 02:19:18 -0800

    capped to 60 fps

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while pygame.init():
@@ -15,11 +19,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
-
-
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print (f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 
