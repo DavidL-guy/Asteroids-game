4ebcf450587b933741a563a844ea162e6b90555e
DavidL-guy <davidlgan392@gmail.com>
2026-01-22 18:59:47 -0800

    asteroids split

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import *
-from constants import LINE_WIDTH
+from constants import *
+from logger import log_event
+import random
 
 class Asteroids(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,18 @@ class Asteroids(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            angle = random.uniform(20, 50)
+            asteroid1_velocity = self.velocity.rotate(angle)
+            asteroid2_velocity = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroids(self.position.x, self.position.y, new_radius)
+            asteroid2 = Asteroids(self.position.x, self.position.y, new_radius)
+            asteroid1.velocity = asteroid1_velocity * 1.2
+            asteroid2.velocity = asteroid2_velocity * 1.2
